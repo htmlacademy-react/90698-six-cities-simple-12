3507f6df1cf8d02da98a83cd7573664a990166cb
vertina61 <natalya.vertieva@mail.ru>
2023-05-02 06:42:17 +0300

    Общее колво комментариев

--- project/src/types/state.ts
+++ project/src/types/state.ts
@@ -5,32 +5,29 @@ import { UserInfo } from './authorization';
 import { Comments } from './comments';
 import { SortingOptions } from './sorting';
 
-export type UserData = {
-    userInfo: Omit<UserInfo, 'token'> | undefined;
-    authorizationStatus: AuthorizationStatus;
-  };
-
 export type OffersData = {
-    offers: Offers;
-    singleOffer: Offer | undefined;
-    notFoundSingleOfferError: boolean;
-    isSingleOfferLoading: boolean;
-    nearbyOffers: Offers;
-    areNearbyOffersLoading: boolean;
-    areOffersLoading: boolean;
-    sortingOptions: SortingOptions;
-    error: boolean;
-  };
-
+  offers: Offers;
+  singleOffer: Offer | undefined;
+  notFoundSingleOfferError: boolean;
+  isSingleOfferLoading: boolean;
+  nearbyOffers: Offers;
+  areNearbyOffersLoading: boolean;
+  areOffersLoading: boolean;
+  sortingOptions: SortingOptions;
+  error: boolean;
+};
 export type CommentsData = {
-    comments: Comments;
-    areCommentsLoading: boolean;
-    isCommentBeingPosted: boolean;
-  }
+  comments: Comments;
+  areCommentsLoading: boolean;
+  isCommentBeingPosted: boolean;
+}
 
 export type CurrentCityData = {
-    currentCity: string;
-  };
-
+  currentCity: string;
+};
+export type UserData = {
+  userInfo: Omit<UserInfo, 'token'> | undefined;
+  authorizationStatus: AuthorizationStatus;
+};
 export type State = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
